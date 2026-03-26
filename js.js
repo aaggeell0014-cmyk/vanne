b6eaa598a3616682a4c6963813469694f98b5ced
aaggeell0014-cmyk <aaggeell0014@gmail.com>
2026-03-26 15:10:55 -0600

    Create js.js

---
+++ js.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const boton = document.getElementById("boton");
+
+    boton.addEventListener("click", function() {
+        document.getElementById("mensaje").innerHTML =
+        "🐱💜 Oye... ¿quieres ir por un jugo conmigo? 🧃✨";
+    });
+});
